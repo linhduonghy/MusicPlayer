c65c8c20393876eff87c29f5f6f40fa8e6314511
linhduonghy <linhduonghy@gmail.com>
2021-03-02 16:09:45 +0700

    change DBquery

--- src/DatabaseQuery/DBQuery.java
+++ src/DatabaseQuery/DBQuery.java
@@ -1,6 +1,5 @@
 package DatabaseQuery;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,8 +7,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DBQuery {
-    
-    private Connection con;      
+
+    private Connection con;
+    private String username = "root";
+    private String password = "linhdv";
+    private String dbName = "music player";
+    private String url = "jdbc:mysql://localhost:3306/" + dbName + "?useSSL=false";
 
     public DBQuery() {
         con = connectDB();
@@ -20,33 +23,26 @@ public class DBQuery {
     }
 
     public Connection connectDB() {
-        Connection con = null;
-        String url = "jdbc:mysql://localhost:3306/music player?useSSL=false";
-        String username = "root";
-        String password = "linhdv";
-        try {            
-            con = DriverManager.getConnection(url, username, password);
-        } catch ( SQLException  e) {            
+        if (con == null) {
+            try {
+                con = DriverManager.getConnection(url, username, password);
+            } catch (SQLException e) {
+            }
         }
         return con;
-    }    
-    
-        
-    public ResultSet query(String sql) throws SQLException {           
+    }
+
+    public ResultSet query(String sql) throws SQLException {
         Statement statement = con.createStatement();
-        ResultSet resultSet = statement.executeQuery(sql);        
+        ResultSet resultSet = statement.executeQuery(sql);
         return resultSet;
     }
-    
+
     public void setConnection(Connection con) {
         this.con = con;
     }
-    
+
     public Connection getConnection() {
         return con;
-    }
-    public static void main(String[] args) {
-        DBQuery db = new DBQuery();
-        System.out.println(db.connectDB());
     }
 }
